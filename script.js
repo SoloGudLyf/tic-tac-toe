d800df926b023aa9aabf32835b96fb6503816f76
SoloGudLyf <solomonteiko0@gmail.com>
2025-10-18 17:08:46 +0000

    Refactor code and include draw condition
    to help in encapsulation and end game when it's draw

--- script.js
+++ script.js
@@ -1,18 +1,18 @@
-const gameBoard = [];
+let gameBoard = [];
 const player1 = { marker: "X" };
 const player2 = { marker: "O" };
-const manageGame = {};
 let playerTurn = 1;
-const getGameBoard = (function () {
-  for (i = 0; i < 3; i++) {
-    gameBoard[i] = [];
-    for (j = 0; j < 3; j++) {
-      gameBoard[i].push([]);
-    }
-  }
-})();
 
-console.log(gameBoard);
+const getGameBoard = (function () {
+    for (i = 0; i < 3; i++) {
+      gameBoard[i] = [];
+      for (j = 0; j < 3; j++) {
+        gameBoard[i].push([]);
+      }
+    }
+  })();
+
+ function gameLogic(){
 
 const printBoard = function () {
   for (const element of gameBoard) {
@@ -23,24 +23,24 @@ const printBoard = function () {
 const playRound = function (row, column) {
   if (gameBoard[row - 1][column - 1] === "O" ||   gameBoard[row - 1][column - 1] === "X"
   ) {
-    console.log("Position Taken")
-    return
+    return "Position Taken"
   }
   if (playerTurn === 1) {
     gameBoard[row - 1][column - 1] = player1.marker;
     console.clear();
     printBoard();
     playerTurn = 2;
+    return "Piece Placed"
   } else {
     gameBoard[row - 1][column - 1] = player2.marker;
     console.clear();
     printBoard();
     playerTurn = 1;
+    return "Piece Placed"
   }
-  console.log(gameBoard[row - 1][column - 1])
 
 };
-
+let winner;
 const checkWinner = function () {
   const winSituations = [
     [
@@ -90,24 +90,48 @@ const checkWinner = function () {
       i[0][0] !== ""
     ) {
       let winnerValue = getValue(i[0][0], i[0][1]);
-      let winner = winnerValue === "X" ? "Player1" : "Player2";
+       winner = winnerValue === "X" ? "Player1" : "Player2";
       return `The winner is ${winner}`;
     }
   }
   return `No winner yet`;
 };
 
+const gameOver = function(){
+    let piecePlaced = 0
+    for (const element of gameBoard) {
+        for (const elem of element) {
+            if (elem === "X" || elem === "O"){
+                piecePlaced++
+            }
+        }
+    }
+    if (checkWinner() === `The winner is ${winner}` ){
+        restartGame()
+        return "Game Over"
+    }
+    else if(checkWinner() !== `The winner is ${winner}` && piecePlaced === 9){
+        return "Game Over, It's a draw"
+    }
+   return "Game Not Over Yet"
+
+}
+
+const restartGame = function(){
+    gameBoard = []
+    for (i = 0; i < 3; i++) {
+        gameBoard[i] = [];
+        for (j = 0; j < 3; j++) {
+          gameBoard[i].push([]);
+        }
+      }
+}
+
 const getValue = function (row, column) {
   return gameBoard[row - 1][column - 1];
 };
+return {printBoard,playRound,checkWinner,getValue,gameOver}
+}
 
-playRound(1, 2);
-playRound(2, 3);
-playRound(2, 3);
-playRound(1, 2);
 
-// playRound(2,3)
-// playRound(3,2)
-// playRound(2,1)
-console.log(gameBoard[1 - 1][2 - 1].length)
-console.log(checkWinner());
+
